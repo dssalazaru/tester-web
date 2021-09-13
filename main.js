7ec81622f4f2b32fcafc2368d89aa81642c9b9cb
dssalazaru <dssalazaru@gmail.com>
2021-09-12 23:09:53 -0500

    Se agregan fuentes de font awesome y se agregan datos al HTML

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// document.getElementById('print').innerHTML = "something important";
+// document.getElementById('print').insertAdjacentHTML('beforeend', "something added");
+
+function changeColor(newColor) {
+    var elem = document.getElementById('para');
+    elem.style.color = newColor;
+  }
+
 var ds = "Daniel Salazar";
 var time = new Date().getHours() + ":" + new Date().getMinutes();
-document.getElementById('hello').document.write("Hola mundo soy " + ds + "\nSon las " + time);
+var hello = document.getElementById('hello');
+hello.innerHTML = "Hola mundo soy " + ds + "\nSon las " + time;
+
+var elem = document.getElementById('hader'),
+    L = 5;
+
+for(var i = 0; i < L; i++) {
+  elem.insertAdjacentHTML('beforeend',
+        "Nombre" +
+        " | " +
+        "Lista" +
+        " | " +
+        "Correo" +
+        "<br><hr><br>"
+  );
+}
