01a66a9b3d102040654d992a206c86647e33a688
dssalazaru <dssalazaru@gmail.com>
2021-09-14 01:04:54 -0500

    Programa hader

--- js/main.js
+++ js/main.js
@@ -10,17 +10,3 @@ var ds = "Daniel Salazar";
 var time = new Date().getHours() + ":" + new Date().getMinutes();
 var hello = document.getElementById('hello');
 hello.innerHTML = "Hola mundo soy " + ds + "\nSon las " + time;
-
-var elem = document.getElementById('hader'),
-    L = 5;
-
-for(var i = 0; i < L; i++) {
-  elem.insertAdjacentHTML('beforeend',
-        "Nombre" +
-        " | " +
-        "Lista" +
-        " | " +
-        "Correo" +
-        "<br><hr><br>"
-  );
-}
