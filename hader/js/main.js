6615349cd474471f67c4cd89b2dcb9c6018c2eb2
dssalazaru <dssalazaru@outlook.com>
2021-09-15 12:46:05 -0500

    Version 2 de app hader

--- hader/js/main.js
+++ hader/js/main.js
@@ -4,12 +4,12 @@ fetch('info.txt') // <-- Ruta del archivo
     .then(text => printdata(text))
 
 function printdata(info) {
-    if (info.length > 0) {
+    if (info.length > 0) { // Imprime en consola o genera aviso
         console.log("Archivo cargado correctamente\nTamaño del archivo: " + info.length);
     } else {
         alert("Archivo vacio");
     }
-    
+
     // Ajuste de datos en Array
     var array = info.split("\n");
     var elem = document.getElementById('data');
@@ -20,19 +20,27 @@ function printdata(info) {
         var owners = array[i].replace(/.* Owners: /, '');
         var n = i + 1
         // Imprimir fila de datos en la tabla
-        if (list.length > 0) {elem.innerHTML += `<tr><th scope="row" id="col1">${n}</th><td>${list}</td><td>${owners}</td></tr>`;}
+        if (list.length > 0) {
+            elem.innerHTML += `<tr><th scope="row" id="col1">${n}</th><td>${list}</td><td>${owners}</td></tr>`;
+        }
     }
+    $(document).ready(function () {
+        $('#tabla').DataTable();
+    });
 }
 
 // Funcion volver arriba
-$(document).ready(function(){
-	$('.up').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 0) {$('.up').slideDown(300);}
-        else {$('.up').slideUp(300);}
-	});
+$(document).ready(function () {
+    $('.up').click(function () {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.up').slideDown(300);
+        } else {
+            $('.up').slideUp(300);
+        }
+    });
 });
